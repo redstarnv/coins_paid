8b38a1b340e6331f30d04c9e01eecde5be0490e7
Artem Biserov <artem.biserov@gmail.com>
2022-03-24 15:38:09 +0300

    remove qr code implementation

--- lib/coins_paid.rb
+++ lib/coins_paid.rb
@@ -3,34 +3,14 @@ require 'dry-struct'
 require 'dry-initializer'
 require_relative 'coins_paid/address'
 require_relative 'coins_paid/coins_paid_address'
-require_relative 'coins_paid/qr_code'
 
 module CoinsPaid
   module_function
 
   UnknownCurrency = Class.new RuntimeError
 
-  DEFAULT_CONVERT_TO = ENV['COINS_PAID_CURRENCY']
-
-  ADDRESS_PREFIXES = {
-    'BTC' => 'bitcoin',
-    'BCH' => 'bitcoincash',
-    'DOGE' => 'doge',
-    'ETH' => 'ethereum',
-    'EURTE' => 'ethereum',
-    'LTC' => 'litecoin',
-    'USDT' => 'bitcoin',
-    'USDTE' => 'ethereum',
-    'TRX' => 'tron',
-    'USDTT' => 'tron'
-  }.freeze
-
-  def address(foreign_id, currency:)
-    Address.new(foreign_id: foreign_id, currency: currency, convert_to: DEFAULT_CONVERT_TO).call
-  end
-
-  def qr_code(player_id, currency:, label:, message:)
-    QrCode.new(player_id, currency: currency, label: label, message: message)
+  def address(foreign_id, currency:, convert_to:)
+    Address.new(foreign_id: foreign_id, currency: currency, convert_to: convert_to).call
   end
 
   def currency(name)
